0f3f561c2fdbfadb350dc41e0645b3c2d9a9e126
Arturs <zaharova@Arturss-MacBook-Air.local>
2021-05-10 21:09:14 +0300

    modal window added

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contacts',
@@ -7,14 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
-  lol = false;
+  mouseOnModal: boolean = false;
+  formSubmitted: boolean = false;
+  modalOpeneded: boolean = false;
 
-  check() {
-    this.lol = true;
+  submitForm() {
+    this.formSubmitted = !this.formSubmitted;
+    this.modalOpeneded = !this.modalOpeneded;
+    setTimeout(() => {
+      this.refreshComponent()
+    }, 10000);
   }
+
+  refreshComponent() {
+    let currentUrl = this.router.url;
+
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.navigate([currentUrl]);
+  }
+
+  detectModalMouseLeave() {
+    this.mouseOnModal = false;
+  }
+
+  detectModalMouseEnter() {
+    this.mouseOnModal = true;
+  }
+
+  processClickOutside() {
+    if (!this.mouseOnModal) {
+      this.modalOpeneded = false;
+      this.refreshComponent()
+    } else {
+      return
+    }
+  }
+
+  showModal() {
+    if (this.modalOpeneded) {
+      return {
+        modal: true,
+        'display': 'fixed'}
+    } else {
+      return {
+        modal: false
+      }
+    }
+  }
+
 }
